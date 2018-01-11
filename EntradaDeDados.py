c01aa1f444479e7e131aef68746f11f9eb959e0f
Danilocl <danilocalessa@gmail.com>
2018-01-11 21:39:59 -0200

    conversão de tipos para String e método len()

---
+++ EntradaDeDados.py
@@ -0,0 +1,11 @@
+n1 = input('Digite o primeiro nome: ')
+n2 = input('Digite o segundo nome: ')
+
+print('\n',n1+n2)
+
+print("\nAgora números")
+
+n1 = float(input('\nDigite o primeiro número: '))
+n2 = float(input('\nDigite o segundo número: '))
+
+print('\n',n1+n2)
---
+++ StrLen.py
@@ -0,0 +1,9 @@
+n1 = 2
+n2 = 100
+n3 = str(n1 ** n2)
+print(n3)
+print(len(n3))
+
+a = str(2 ** 100000)
+b = len(a)
+print(b)
